597f916e2409552b63ed43dfc3b30a8182ab01eb
EfrataAron <ephretaselasie@gmail.com>
2025-02-21 16:58:10 +0300

    Paas cloud project

--- main.py
+++ main.py
@@ -27,13 +27,19 @@
 #     ],
 #     debug=True,
 # )
-from flask import Flask
+
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return "Hello, World! BSSE"
+    return render_template("index.html")  # Serve the HTML file
+    #return "Hello, World! BSSE"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+     app.run(host='0.0.0.0', port=8080)
+    
+    
+
